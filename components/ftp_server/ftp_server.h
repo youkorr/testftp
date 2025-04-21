f014abd5608a027f78320ad8f24d66a668339055
youkorr <96953496+youkorr@users.noreply.github.com>
2025-04-21 20:24:21 +0200

    Update ftp_server.h

--- components/ftp_server/ftp_server.h
+++ components/ftp_server/ftp_server.h
@@ -22,9 +22,6 @@ class FTPServer : public Component {
   void loop() override;
   void dump_config() override;
 
-  
-
-  // Définir une priorité d'initialisation tardive pour ESP-IDF
   float get_setup_priority() const override { return setup_priority::LATE - 1; }
 
   void set_port(uint16_t port) { port_ = port; }
@@ -32,18 +29,18 @@ class FTPServer : public Component {
   void set_password(const std::string &password) { password_ = password; }
   void set_root_path(const std::string &root_path) { root_path_ = root_path; }
 
-  // Méthode pour vérifier si le serveur est en cours d'exécution
   bool is_running() const;
 
  protected:
   void handle_new_clients();
   void handle_ftp_client(int client_socket);
   void remove_ftp_client(int client_socket);
+  void cleanup_client(int client_socket);  // Added cleanup_client declaration
   void process_command(int client_socket, const std::string& command);
   void send_response(int client_socket, int code, const std::string& message);
   bool authenticate(const std::string& username, const std::string& password);
   void list_directory(int client_socket, const std::string& path);
-  void list_names(int client_socket, const std::string& path);  // Add this line
+  void list_names(int client_socket, const std::string& path);
   void start_file_upload(int client_socket, const std::string& path);
   void start_file_download(int client_socket, const std::string& path);
 
@@ -58,16 +55,13 @@ class FTPServer : public Component {
   std::vector<std::string> client_usernames_;
   std::vector<std::string> client_current_paths_;
 
-  // Variables pour le mode passif
   bool passive_mode_enabled_ = false;
   int passive_data_socket_ = -1;
   int passive_data_port_ = -1;
   std::string passive_client_ip_;
 
-  // Variable pour la commande RNFR
-  std::string rename_from_;  // Add this line
+  std::string rename_from_;
 
-  // Méthodes pour le mode passif
   bool start_passive_mode(int client_socket);
   int open_data_connection(int client_socket);
   void close_data_connection(int client_socket);
